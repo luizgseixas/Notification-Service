a545adfa144d154c1282bcf0bcaed67e53e34e9e
luiz Seixas <ticoluizgustavo@outlook.com>
2022-12-16 11:23:39 -0300

    feat: controller routes

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { NotificationsRepository } from '@application/repositories/notifications-repository';
 import { PrismaService } from './prisma/prisma.service';
-import { PrismaNotificationsRepository } from './repositories/PrismaNotificationsRepository';
+import { PrismaNotificationsRepository } from './repositories/prisma-notifications-repository';
 
 @Module({
   providers: [
